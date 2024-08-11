89eb6c2dc0cc63faf4f9470d9dca9d3d451ac44a
marvillage <122214707+marvillage@users.noreply.github.com>
2024-08-11 20:26:47 +0530

    first commit

---
+++ background_removal_video.py
@@ -0,0 +1,27 @@
+
+
+import numpy as np
+import cv2 as cv
+cap = cv.VideoCapture(r"C:\Users\Kushagra\Downloads\vid.mp4")
+
+
+algo1 = cv.createBackgroundSubtractorMOG2(detectShadows=True) 
+algo2 = cv.createBackgroundSubtractorKNN(detectShadows=True)  
+while True:
+    ret, frame = cap.read()
+    frame = cv.resize(frame,(600,400))
+    if frame is None:
+        break
+    res1 = algo1.apply(frame)
+    res2 = algo2.apply(frame)
+    
+
+    cv.imshow('original', frame)
+    cv.imshow('res1',res1)
+    cv.imshow('res2',res2)
+
+    keyboard = cv.waitKey(60)
+    if keyboard == 'q' or keyboard == 27:
+        break
+cap.release()
+cv.destroyAllWindows()
